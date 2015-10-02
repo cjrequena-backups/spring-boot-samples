af9e6d6f7c4d0ee45b6002c9c99dfd5e2c07e2c2
Carlos Requena <crequena@hotelbeds.com>
2015-10-02 12:33:22 +0200

    sample-spring-boot-simple - Initial commit

---
+++ sample-spring-boot-simple/src/main/java/com/sample/SampleApp.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SampleApp {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleApp.class, args);
+	}
+}
