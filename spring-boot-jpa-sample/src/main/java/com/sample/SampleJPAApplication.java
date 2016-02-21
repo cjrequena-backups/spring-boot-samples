9b055a08108d590c5964c190128674f1ca462282
cjrequena <cjrequena@192.168.1.39>
2016-02-21 16:19:21 +0100

    .

---
+++ spring-boot-jpa-sample/src/main/java/com/sample/SampleJPAApplication.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJPAApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleJPAApplication.class, args);
+	}
+}
