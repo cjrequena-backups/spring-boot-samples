11d91ff08ca7997967f7a54ed9f266e8457e9f41
Carlos Requena <crequena@hotelbeds.com>
2016-02-29 09:29:27 +0100

    .

--- spring-boot-metrics-dropwizard-sample/src/main/java/com/sample/metrics/dropwizard/SampleController.java
+++ spring-boot-metrics-dropwizard-sample/src/main/java/com/sample/metrics/dropwizard/SampleController.java
@@ -1,11 +1,13 @@
-
 package com.sample.metrics.dropwizard;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import org.coursera.metrics.datadog.model.DatadogCounter;
+import org.coursera.metrics.datadog.model.DatadogGauge;
 import org.hibernate.validator.constraints.NotBlank;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.context.annotation.Description;
@@ -13,6 +15,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 
 @Controller
 @Description("A controller for handling requests for hello messages")
@@ -24,10 +32,53 @@ public class SampleController {
 	@Autowired
 	private GaugeService gauges;
 
+	@Autowired
+	private MetricRegistry registry;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
 	public Map<String, String> hello() {
-		this.gauges.submit("test.metrics.dropwizard.crequena", Math.random() * 1000 + 1000);
+
+		// A counter is a simple incrementing and decrementing 64-bit integer:
+		Counter pendingJobs = registry.counter(MetricRegistry.name(SampleController.class, "PROJECT_NAME", "counter"));
+
+		// A Histogram measures the distribution of values in a stream of data:
+		// e.g., the number of results returned by a search:
+		Histogram histogram = registry.histogram(MetricRegistry.name(SampleController.class, "PROJECT_NAME", "histogram"));
+
+		// A meter measures the rate of events over time (e.g., “requests per
+		// second”). In addition to the mean rate, meters also track 1-, 5-, and
+		// 15-minute moving averages.
+		Meter meter = registry.meter(MetricRegistry.name(SampleController.class, "PROJECT_NAME", "meter"));
+
+		// A timer is basically a histogram of the duration of a type of event
+		// and a meter of the rate of its occurrence.
+		final Timer responses = registry.timer(MetricRegistry.name(SampleController.class, "PROJECT_NAME", "timer"));
+
+		pendingJobs.inc();
+		histogram.update(20000);
+		meter.mark();
+		Timer.Context context = responses.time();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		context.stop();
+
+		// this.gauges.submit("test.metrics.dropwizard.crequena", Math.random() * 1000 + 1000);
+
+		List<String> tags = new ArrayList<String>();
+		tags.add("env:prod");
+		tags.add("version:1.0.0");
+		DatadogCounter counter = new DatadogCounter("PEPE", 1L, 1234L, "crequena-hbeds", tags);
+
+		tags = new ArrayList<String>();
+		tags.add("env:prod");
+		tags.add("version:1.0.0");
+		DatadogGauge gauge = new DatadogGauge("PEPE", 1L, 1234L, "crequena-hbeds", tags);
+
 		return Collections.singletonMap("message", this.sampleService.getHelloMessage());
 	}
 
