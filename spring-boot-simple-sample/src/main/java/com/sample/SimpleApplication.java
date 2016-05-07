0db5996c955f66c5d68e141e5bdde09acc17f054
Carlos José Requena Jiménez <carlosjose.requena@gmail.com>
2016-05-08 01:03:38 +0200

    Add module elasticsearch-sample

--- spring-boot-simple-sample/src/main/java/com/sample/SimpleApplication.java
+++ spring-boot-simple-sample/src/main/java/com/sample/SimpleApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class SampleBootMainApp {
+public class SimpleApplication {
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SampleBootMainApp.class, args);
+		SpringApplication.run(SimpleApplication.class, args);
 	}
 }
