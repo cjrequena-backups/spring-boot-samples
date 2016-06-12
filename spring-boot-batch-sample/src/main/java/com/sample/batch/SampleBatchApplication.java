41c5ea0a6ec206d7b5b4ecce0d97c7d7d330986f
Carlos José Requena Jiménez <carlosjose.requena@gmail.com>
2016-06-12 17:58:15 +0200

    sample spring boot batch

---
+++ spring-boot-batch-sample/src/main/java/com/sample/batch/SampleBatchApplication.java
@@ -0,0 +1,14 @@
+package com.sample.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by cjrequena on 12/06/16.
+ */
+@SpringBootApplication
+public class SampleBatchApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleBatchApplication.class, args);
+    }
+}
