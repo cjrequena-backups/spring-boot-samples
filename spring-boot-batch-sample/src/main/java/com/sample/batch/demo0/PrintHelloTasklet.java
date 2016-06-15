92c8e6d5ff8a9ec0bf2fa3f4abc03cf445ad5b3f
Carlos José Requena Jiménez <carlosjose.requena@gmail.com>
2016-06-15 19:47:28 +0200

    .

---
+++ spring-boot-batch-sample/src/main/java/com/sample/batch/demo0/PrintHelloTasklet.java
@@ -0,0 +1,22 @@
+package com.sample.batch.demo0;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class PrintHelloTasklet implements Tasklet {
+
+
+
+
+    public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
+        System.out.print("Hello ");
+        return RepeatStatus.FINISHED;
+    }
+
+}
