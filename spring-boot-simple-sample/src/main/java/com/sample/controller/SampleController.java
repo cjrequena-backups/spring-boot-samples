ac6d034f3dc47f694cd314f650b6dd93f55d4d3b
Carlos Requena <crequena@hotelbeds.com>
2016-09-28 10:29:20 +0200

    adding sleuth trace to spring-boot-simple-sample

--- spring-boot-simple-sample/src/main/java/com/sample/controller/SampleController.java
+++ spring-boot-simple-sample/src/main/java/com/sample/controller/SampleController.java
@@ -1,5 +1,8 @@
 package com.sample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.SpanAccessor;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class SampleController {
+	@Autowired
+	private Tracer tracer;
+
+	@Autowired
+	private SpanAccessor accessor;
+
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
