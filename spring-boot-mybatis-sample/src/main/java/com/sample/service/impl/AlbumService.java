83f944e8936e7e1815fa8904f69bea8009fe65cc
Carlos Requena <crequena@hotelbeds.com>
2016-11-16 17:47:07 +0100

    mybatis sample

--- spring-boot-mybatis-sample/src/main/java/com/sample/service/impl/AlbumService.java
+++ spring-boot-mybatis-sample/src/main/java/com/sample/service/impl/AlbumService.java
@@ -7,6 +7,8 @@ import com.sample.service.IAlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.List;
@@ -23,42 +25,44 @@ public class AlbumService implements IAlbumService, Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Override
+	@Transactional(propagation= Propagation.REQUIRED, readOnly=true, rollbackFor=Exception.class)
 	public long countAll() throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false, rollbackFor=Exception.class)
 	public void create(AlbumEntity entity) throws Exception {
-		// TODO Auto-generated method stub
-
+		this.albumMapper.insert(entity);
 	}
 
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false, rollbackFor=Exception.class)
 	public void update(AlbumEntity entity) throws Exception {
-		// TODO Auto-generated method stub
-
+		this.albumMapper.updateByPrimaryKey(entity);
 	}
 
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false, rollbackFor=Exception.class)
 	public void delete(AlbumEntity entity) throws Exception {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false, rollbackFor=Exception.class)
 	public void deleteByPk(Integer pk) throws Exception {
-		// TODO Auto-generated method stub
-
+		this.albumMapper.deleteByPrimaryKey(pk);
 	}
 
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=true, rollbackFor=Exception.class)
 	public AlbumEntity findByPk(Integer pk) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return this.albumMapper.selectByPrimaryKey(pk);
 	}
 
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=true, rollbackFor=Exception.class)
 	public List<AlbumEntity> findAll() throws Exception {
 		return this.albumMapper.selectAll();
 	}
